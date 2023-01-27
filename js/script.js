44179fdcc2ecc162fd856ea9bf8118c00a4929bd
Paul Morris <creative2012@hotmail.com>
2023-01-27 20:18:51 +0000

    added logic to get weather data and function to add to page

--- js/script.js
+++ js/script.js
@@ -1,46 +1,53 @@
-$("#search").on("click", function (event) {
-    event.preventDefault();
-    getCityCoords();
 
-});
+function getCityCoords(city) {
+    let baseURL = "http://api.openweathermap.org/geo/1.0/direct?"
+    let key = "&appid=df399c578ec151e1ad2bd5287c70f3b8";
+    let query = "q=" + city;
 
-// function getWeather(coords){
-//     var baseURL = "https://api.openweathermap.org/data/2.5/forecast?"
-//     var key ="&api-key=df399c578ec151e1ad2bd5287c70f3b8";
-//     var lat = "q=" + coords[0].lat;
-//     var lon = ''+ coords[0].lon;
-//     var name = coords[0].name;
-//     console.log(coords);
-//     var queryURL = baseURL + query + key;
-//     getData(queryURL, type)
-   
+    let queryURL = baseURL + query + key;
+    getData(queryURL, 'coords');
 
-// }
-function getCityCoords(){
-    var baseURL = "http://api.openweathermap.org/geo/1.0/direct?"
-    var key ="&appid=df399c578ec151e1ad2bd5287c70f3b8";
-    var query = "q=";
-
-    query += $('#query').val();
-
-    var queryURL = baseURL + query + key;
-    getData(queryURL, 'coords')
-        
-    
 }
 
-function getData(queryURL, type){
+function getWeather(coords) {
+    let baseURL = "https://api.openweathermap.org/data/2.5/forecast?"
+    let key = "&appid=df399c578ec151e1ad2bd5287c70f3b8";
+    let lat = "lat=" + coords[0].lat;
+    let lon = '&lon=' + coords[0].lon;
+    let units = '&units=metric'
 
+    let queryURL = baseURL + lat + lon + key + units ;
+    getData(queryURL, 'weather');
+
+}
+
+function popPage(data){
+    console.log(data);
+
+    //city name / date / temp / wind / humidity
+}
+
+function getData(queryURL, type) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
-        if(type == 'coords'){
+    }).then(function (response) {
+        if (type == 'coords') {
             getWeather(response);
-        } else {
-
+        } else if (type == 'weather')  {
+            popPage(response);
         }
-       
-        });
+
+    });
 
 }
+
+function saveData(){
+
+}
+
+$("#search").on("click", function (event) {
+    event.preventDefault();
+    getCityCoords($('#query').val());
+
+});
