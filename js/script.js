d19c8562f49f782df6f6390965352f1ff23e105e
Paul Morris <creative2012@hotmail.com>
2023-01-28 00:08:04 +0000

    added call for current weather data and consolidated with forcast in one function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const key = "&appid=df399c578ec151e1ad2bd5287c70f3b8";
-var first = true;
+const today = $('#weather-view');
+const forcast = $('#weather-forcast');
 //function to get city coordinates from city name
 function getCityCoords(city) {
     let baseURL = "http://api.openweathermap.org/geo/1.0/direct?"
@@ -11,61 +12,86 @@ function getCityCoords(city) {
 }
 
 //function to get weather data from coordinates
-function getWeather(coords) {
-    let baseURL = "https://api.openweathermap.org/data/2.5/forecast?"
+function getWeather(coords){
+    current = true;
+    let baseURL = "https://api.openweathermap.org/data/2.5/weather?"
     let lat = "lat=" + coords.lat;
     let lon = '&lon=' + coords.lon;
     let units = '&units=metric'
 
     let queryURL = baseURL + lat + lon + key + units;
-    getData(queryURL, 'weather');
+    getData(queryURL, 'current');
+
+    baseURL = "https://api.openweathermap.org/data/2.5/forecast?"
+    queryURL = baseURL + lat + lon + key + units;
+    getData(queryURL, 'forcast');
+    
 
 }
 
 //function to add data to page
-function popWeatherResult(dataSet) {
-    let city = dataSet.city.name
-    for (i = 0; i < 6; i++) {
+function popWeatherResult(dataSet,type) {
+    if(type == 'current'){
+        //clear results
+        today.html('');
+        populatePage(dataSet, type, null);
+    } else {
+        console.log(dataSet)
+        //clear results
+        forcast.html('');
+        for (i = 0; i < 40; i = i+ 8) {
+            populatePage(dataSet, type, i)
+        };
+    }
+    
+}
+
+function populatePage(dataSet, type, i){
+    
+    var data;
+    var heading;
+    var target;
+    var className;
+
+    if(type == 'current') {
+        var city = dataSet.name;
+        target = today;
+        className = 'main-data';
+        data = dataSet
+    } else {
         data = dataSet.list[i];
-        //get main data sources
-        let iconDataLocation = data.weather[0];
-        let weatherDataLocation = data;
-        let date = weatherDataLocation.dt_txt;
-        //get icon image and alt
-        let iconcode = iconDataLocation.icon;
-        let alt = iconDataLocation.main;
-        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        //get weather data
-        let temp = weatherDataLocation.main.temp;
-        let wind = weatherDataLocation.wind.speed;
-        let humidity = weatherDataLocation.main.humidity;
-        //check if item to display is today if so set variables
-        var heading = `<div class="date">${moment(date).format('l')} </div>`;
-        var target = $('#weather-forcast');
-        var className = 'forcast-data';
-        if (first) {
-            heading = `<h2 class="city">${city}<span> ${moment(date).format('MMMM Do YYYY')}</span><img src="${iconurl}"  alt="${alt}"></h2>`;
-            target = $('#weather-view');
-            className = 'main-data';
-            first = false;
-        } else {
+        var date = data.dt_txt;
+        target = forcast;
+        className = 'forcast-data';
+    }
 
-        }
-        //create elelemtent and set attributes and inner html
-        let weatherElement = $('<div>');
-        weatherElement.attr('class', className);
-        weatherElement.html(`
-        ${heading}
-        <div class="temp">Temp: ${temp}</div>
-        <div class="wind">Wind: ${wind}</div>
-        <div class="humidity">Humidity: ${humidity}</div>
-        `);
-        //add to page
-        target.append(weatherElement);
+    let iconDataLocation = data.weather[0];
+    //get icon image and alt
+    let iconcode = iconDataLocation.icon;
+    let alt = iconDataLocation.main;
+    let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    //get weather data
+    let temp = data.main.temp;
+    let wind = data.wind.speed;
+    let humidity = data.main.humidity;
+    if(type == 'current') {
+        heading = `<h2 class="city">${city}<span> ${moment().format('MMMM Do YYYY')}</span><img src="${iconurl}"  alt="${alt}"></h2>`;
+    } else {
+        heading = `<div class="date">${moment(date).format('l')} </br> <img src="${iconurl}"  alt="${alt}"></div>`;
+    }
+    //create elelemtent and set attributes and inner html
+    let weatherElement = $('<div>');
+    weatherElement.attr('class', className);
+    weatherElement.html(`
+    ${heading}
+    <div class="temp">Temp: ${temp}</div>
+    <div class="wind">Wind: ${wind}</div>
+    <div class="humidity">Humidity: ${humidity}</div>
+    `);
+    //add to page
+    target.append(weatherElement);
+    
 
-
-    };
-    first = true;
 }
 
 //function to call ajax request
@@ -77,11 +103,11 @@ function getData(queryURL, type) {
         if (type == 'coords') {
             getWeather(response[0]);
 
-        } else if (type == 'weather') {
+        } else {
+           
+            popWeatherResult(response, type);
 
-            popWeatherResult(response);
-
-        }
+        } 
 
     });
 
@@ -98,3 +124,6 @@ $("#search").on("click", function (event) {
     getCityCoords($('#query').val());
 
 });
+
+
+
